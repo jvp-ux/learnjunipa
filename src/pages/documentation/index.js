3d9435cfdbe44c11ee3e5549da53670d72548a35
Jason Roberts <jason@vastpuddle.com.au>
2020-02-27 20:45:20 +1100

    documentation bg chagne

--- src/pages/documentation/index.js
+++ src/pages/documentation/index.js
@@ -10,7 +10,7 @@ export default class DocumentationIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            backgroundImage: `url('/img/bg-documentation.svg')`,
           }}
         >
           <h1
