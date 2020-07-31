0b29ce44de04f445a725121d1ee9efd7a128bb11
Jason Roberts <jason@vastpuddle.com.au>
2020-07-31 14:34:06 +1000

    push changes

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -123,8 +123,7 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         description
-        tags
-        uniqueClass
+        tags        
       }
     }
   }
